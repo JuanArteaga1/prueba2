814a38b602887fd97612558a4306c18697911a59
JuanArteaga1 <166939514+JuanArteaga1@users.noreply.github.com>
2024-05-21 15:33:30 -0500

    Add files via upload

---
+++ Numprimos.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numprimos;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jumaa
+ */
+public class Numprimos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int [] primos = {2,3,5,7,11,13,17,23,29};
+        
+        
+        for (int i = 0; i < primos.length; i++) {
+            System.out.println(primos[i]);
+            
+        }
+        System.out.println(Arrays.toString(primos));
+        
+        
+       
+    }
+    
+}
